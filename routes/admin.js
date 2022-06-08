a9e6b10ed0c6da87939297f918ae6047c5b829fc
Dkboss743 <2019246@iiitdmj.ac.in>
2022-06-08 16:30:19 +0530

    Adding more routes

--- routes/admin.js
+++ routes/admin.js
@@ -9,5 +9,5 @@ router.get("/add-product", adminController.getAddProduct);
 
 // /admin/add-product => POST
 router.post("/add-product", adminController.postAddProduct);
-router.get("/products");
+router.get("/products", adminController.getProducts);
 exports.routes = router;
