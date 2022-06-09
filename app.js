6c014b76c064caef0bd120cb0b9036c6add5da9d
Dkboss743 <2019246@iiitdmj.ac.in>
2022-06-09 13:10:10 +0530

    style changes

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const path = require("path");
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const { engine } = require("express-handlebars");
@@ -17,11 +16,11 @@ app.set("view engine", "ejs");
 app.set("views", "views");
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const { request } = require("http");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/admin", adminRoutes.routes);
 app.use(shopRoutes.routes);
 app.use(errorController.get404);
-
 app.listen(3000);
