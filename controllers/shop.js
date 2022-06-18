5f86b9e682d9b92a0835b7e406025af480bc07dd
Dkboss743 <2019246@iiitdmj.ac.in>
2022-06-18 16:06:18 +0530

    adding orders

--- controllers/shop.js
+++ controllers/shop.js
@@ -99,11 +99,61 @@ exports.getCheckout = (req, res, next) => {
     pageTitle: "Checkout",
   });
 };
+exports.postCartDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+
+  req.user
+    .getCart()
+    .then((cart) => {
+      return cart.getProducts({ where: { id: prodId } });
+    })
+    .then((products) => {
+      const product = products[0];
+      return product.cartItem.destroy();
+    })
+    .then((result) => {
+      res.redirect("/cart");
+    })
+    .catch((err) => console.log(err));
+};
+exports.getOrders = (req, res, next) => {
+  res.render("shop/orders", {
+    path: "/orders",
+    pageTitle: "Your Orders",
+  });
+};
 
 exports.postCart = (req, res, next) => {
+  let fetchedCart;
+  let newQuantity = 1;
   const prodId = req.body.productId;
-  Product.findById(prodId, (product) => {
-    Cart.addProduct(prodId, product.price);
-  });
-  res.redirect("/cart");
+  req.user
+    .getCart()
+    .then((cart) => {
+      fetchedCart = cart;
+      return cart.getProducts({ where: { id: prodId } });
+    })
+    .then((products) => {
+      let product;
+      if (products.length > 0) {
+        product = products[0];
+      }
+      if (product) {
+        const oldQuantity = product.cartItem.quantity;
+        newQuantity = oldQuantity + 1;
+        return product;
+      }
+      return Product.findByPk(prodId);
+    })
+    .then((product) => {
+      return fetchedCart.addProduct(product, {
+        through: {
+          quantity: newQuantity,
+        },
+      });
+    })
+    .then(() => res.redirect("/cart"))
+    .catch((err) => {
+      console.log(err);
+    });
 };
